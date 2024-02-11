ca363cfd00512a5205687bfced7828bb93f6c386
MartaKasprzyk <martha.kasprzyk@gmail.com>
2024-02-11 13:25:14 +0100

    added try/execept to make sure user input is correct

--- fibonacci.py
+++ fibonacci.py
@@ -4,15 +4,19 @@ def fib_recursive(n):
     :param n: user input, the number of the element to show
     :return: the n-th element of the Fibonacci
     """
-    if type(n) is int:
-        if n < 2:
-            return n
+    try:
+        n = int(n)
+        if n >= 0:
+            if n < 2:
+                return n
+            else:
+                a, b = n - 1, n - 2
+                return fib_recursive(a) + fib_recursive(b)
         else:
-            a, b = n - 1, n - 2
-            return fib_recursive(a) + fib_recursive(b)
-    else:
-        return "Input integer only!"
+            return "Incorrect input. Input must be a integer >= 0."
+    except ValueError:
+        return "Incorrect input. Input must be a integer >= 0."
 
 
-num = int(input("Enter n: "))
+num = input("Enter n: ")
 print(fib_recursive(num))
